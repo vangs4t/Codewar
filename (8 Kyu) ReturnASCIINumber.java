3a9d505350c8080646638ce824d0860ee77f5874
Jhon <132740229+vangs4t@users.noreply.github.com>
2023-07-26 20:08:33 +0700

    Create (8 Kyu) ReturnASCIINumber.java

---
+++ (8 Kyu) ReturnASCIINumber.java
@@ -0,0 +1,14 @@
+public class ASCII {
+    /**
+     * Mengimput nomor dan mengembalikan huruf yang sesuai dengan ASCII
+     * @param c
+     * @return
+     */
+    public static char getChar(int c) {
+        return (char) c;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getChar(65));
+    }
+}
